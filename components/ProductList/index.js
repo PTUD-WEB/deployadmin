3160822dc42a5ca140b43359530f149341a9bc22
KhoaMinHi <minhkhoa031099@gmail.com>
2022-01-17 22:50:12 +0700

    Error: Cannot find module ./productListController

--- components/ProductList/index.js
+++ components/ProductList/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const ProductController = require('./productListController')
+const ProductController = require('./ProductListController')
 router.use(express.static('public'));
 router.get('/', ProductController.productList)
 //ProductList - quan ly he thong gian hang
