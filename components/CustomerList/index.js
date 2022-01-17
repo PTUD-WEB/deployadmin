fcc303c793948aa2da8f90957bcc6e7ae52010d6
Huu Hoang <18120387@student.hcmus.edu.vn>
2022-01-17 16:36:00 +0700

    ProductDetail - CustomerList

--- components/CustomerList/index.js
+++ components/CustomerList/index.js
@@ -4,5 +4,5 @@ var router = express.Router();
 const CustomerListController = require('./CustomerListController')
 router.use(express.static('public'));
 router.get('/', CustomerListController.CustomerList)
-// router.get('/:id', CustomerListController.productByCategories)
+//ProductDetail
 module.exports = router;
