9a4be12e1dd5bdde415b13dcea02bd81a6f3f548
KhoaMinHi <minhkhoa031099@gmail.com>
2022-01-17 20:24:17 +0700

    admin oke

--- models/index.js
+++ models/index.js
@@ -3,20 +3,21 @@ const { Sequelize } = require('sequelize');
 
 const initModels = require('./init-models');
 //const connPostgresString = process.env.DB_URL;//add require('dotenv').config(); into www
-//const connPostgresString = 'postgres://wxjfftihybqpun:584926c1c528115d979e1e2cd8ac86a41acc3da6b44eaefdb5be61662790a3de@ec2-3-227-149-67.compute-1.amazonaws.com:5432/d75gjnh8aca4q1';
-const connPostgresString = 'postgres://postgres:241775456@localhost:5432/postgres';
-const sequelize = new Sequelize(connPostgresString, {
+const connPostgresString = 'postgres://wxjfftihybqpun:584926c1c528115d979e1e2cd8ac86a41acc3da6b44eaefdb5be61662790a3de@ec2-3-227-149-67.compute-1.amazonaws.com:5432/d75gjnh8aca4q1';
+//const connPostgresString = 'postgres://postgres:KJPAMI@localhost:5432/postgres';
+/*const sequelize = new Sequelize(connPostgresString, {
   dialect: 'postgres',
-});
-// const sequelize = new Sequelize(connPostgresString, {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//     }
-//     }
-//   });
+});*/
+
+const sequelize = new Sequelize(connPostgresString, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
+    }
+  });
 module.exports = {
   sequelize,
   models: initModels(sequelize),
